679719b0e3d225201250bc77882d82b10f8b9f70
Valentin Kongar <kongar_v@tass.ru>
2016-09-06 15:35:16 +0300

    flux begin

---
+++ app/components/App.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import uuid from 'uuid';
+import Notes from './Notes';
+import connect from '../libs/connect';
+import NoteActions from '../actions/NoteActions';
+
+class App extends React.Component {
+
+    render(){
+        const {notes} = this.props;
+
+        return (
+            <div>
+                <button className="add-note" onClick={this.addNote}>+</button>
+                <Notes
+                    notes={notes}
+                    onNoteClick={this.activateNoteEdit}
+                    onEdit={this.editNote}
+                    onDelete={this.deleteNote}
+                    />
+            </div>
+        );
+    }
+
+    addNote = () => {
+        this.props.NoteActions.create({
+            id: uuid.v4(),
+            task: 'New task ' + uuid.v4()
+        });
+    }
+
+    deleteNote = (id, e) => {
+        e.stopPropagation();
+        this.props.NoteActions.delete(id);
+    }
+
+    activateNoteEdit = (id) => {
+        this.props.NoteActions.update({id, editing: true});
+    }
+
+    editNote = (id, task) => {
+        this.setState({
+            notes: this.state.notes.map(note => {
+                if (note.id === id) {
+                    note.editing = false;
+                    note.task = task;
+                }
+
+                return note;
+            })
+        });
+    }
+}
+
+export default connect(({notes}) => ({
+    notes
+}), {
+    NoteActions
+})(App)
